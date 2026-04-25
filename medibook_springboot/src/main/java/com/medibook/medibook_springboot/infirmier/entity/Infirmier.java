71dafd6dc0a3172f36ea43d5b98771018b241f60
Pone-Lou <tschivapone@gmail.com>
2026-04-25 14:40:35 +0000

    mon travail avant pull

--- medibook_springboot/src/main/java/com/medibook/medibook_springboot/infirmier/entity/Infirmier.java
+++ medibook_springboot/src/main/java/com/medibook/medibook_springboot/infirmier/entity/Infirmier.java
@@ -1,4 +1,5 @@
 package com.medibook.medibook_springboot.infirmier.entity;
+import com.medibook.medibook_springboot.auth.entity.Utilisateur;
 
-public class Infirmier {
+public class Infirmier extends Utilisateur{
 }
