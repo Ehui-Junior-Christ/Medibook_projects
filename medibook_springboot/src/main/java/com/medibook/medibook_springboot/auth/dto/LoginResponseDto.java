a16af2c15d58ca2a355f41a11d0867b47da85567
Ehui-Junior-Christ <juniorehui15@gmail.com>
2026-04-29 20:31:52 +0000

    tt regler

--- medibook_springboot/src/main/java/com/medibook/medibook_springboot/auth/dto/LoginResponseDto.java
+++ medibook_springboot/src/main/java/com/medibook/medibook_springboot/auth/dto/LoginResponseDto.java
@@ -3,12 +3,27 @@ package com.medibook.medibook_springboot.auth.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDto {
 
     private Long id;
     private String nom;
     private String prenom;
     private String role;
+    
+    private String cmu;
+    private String telephone;
+    private String email;
+    private String photoProfil;
+    
+    private String matricule;
+    private String specialiteMedicale;
+    private String service;
+    
+    private String sexe;
+    private String dateNaissance;
 }
